526d1e3fe1ddaf9a997d0bc020a7a242cb7aa5ae
tomebore <akylaimanalieva@gmail.com>
2020-08-13 00:07:58 +0600

    superuser crated , products model added , product views , products.html

--- product/views.py
+++ product/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from product.models import *
 
-# Create your views here.
+def products(request):
+    context = {}
+    context["products"] = Product.objects.filter(avialable=True)
+    return render(request ,"product/products.html" ,context)
